210c0c9e96fdcfe485931b95f3b7cc5b24a73726
hashash11 <101027054+hashash11@users.noreply.github.com>
2022-09-26 16:23:46 -0400

    Add files via upload

---
+++ Filter/filters.ts
@@ -0,0 +1,17 @@
+export interface Filter{
+
+    Country: string,
+    Perigee: string, 
+    Period: string, 
+    Inclination: string, 
+    Object_Type: string, 
+    Start_Date: string, 
+    End_Date:string,
+    Location: string,
+    Speed: number, 
+    Debris: boolean,
+    Beam: boolean, 
+    Instruments: boolean,
+    Follow_Earth: boolean,
+    Auto_Refresh: boolean
+}
